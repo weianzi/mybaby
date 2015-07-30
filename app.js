08e22a585336bf094e9963bc10d97a64eb8f8887
weianzi <ancool@foxmail.com>
2015-07-30 12:59:30 +0800

    2

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ var express = require("express");
 var path = require("path");
 var port = process.env.PORT || 8080;
 var app = express();
-
 var cookieParser = require("cookie-parser");
 var session = require("express-session");
 var serveStatic = require("serve-static");
-
 var mongoose = require("mongoose");
 var mongoStore = require("connect-mongo")(session);
 var dbUrl = "mongodb://localhost/ancool";
-mongoose.connect(dbUrl);
-
 var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
+
+app.set("view engine", "jade");
+app.set("views", "./app/views");
+app.use(serveStatic(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+mongoose.connect(dbUrl);
 
 app.use(cookieParser());
 app.use(session({
@@ -29,6 +29,8 @@ app.use(session({
 	})
 }));
 
+app.locals.moment = require("moment");
+
 if("development" === app.get("env")){
 	app.set("showStackError", true);
 	//app.use(express.logger(":method :url :status"));
@@ -36,11 +38,5 @@ if("development" === app.get("env")){
 }
 
 require("./routes")(app);
-
-app.set("views", "./app/views");
-app.set("view engine", "jade");
-app.use(serveStatic(path.join(__dirname, "public")));
-app.locals.moment = require("moment");
 app.listen(port);
-
-console.log("mybaby started on port" + port);
+console.log("mybaby started on port:" + port);
